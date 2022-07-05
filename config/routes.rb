f65b9dd31d693cf3ede57bd7fce4157378577ece
sarii0213 <anjing.singka@gmail.com>
2022-07-05 12:24:01 +0900

    :sparkles: Add question & logout feature
    質問機能・ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :users
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :questions
 end
