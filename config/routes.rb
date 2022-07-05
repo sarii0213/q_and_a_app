2bacebabbf0e1a8bae09a48b1ff14ce9407b04fd
sarii0213 <anjing.singka@gmail.com>
2022-07-05 13:23:30 +0900

    :sparkles: Add answer feature
    回答機能（作成・削除）の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :questions
+  resources :questions do
+    resources :answers, only: [:create, :destroy]
+  end
 end
